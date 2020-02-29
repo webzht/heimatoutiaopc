246c82fa6c34d3ed1a70be6e3283a9ea0257215b
tengtengcai <2373099718@qq.com>
2020-02-29 17:11:14 +0800

    初建登录和主页

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Home from '@/views/home'
+import login from '@/views/login' // 简写形式 同理 @/views/login/index.vue
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    redirect: '/home' // 强制跳转到home页
+  },
+  {
+    path: '/home',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'login',
+    component: login
   }
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
